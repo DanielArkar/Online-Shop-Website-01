c8b3ad343dcdddc9f0f9f8895762ec581203b006
Daniel <danielarkar36@gmail.com>
2023-01-25 12:39:05 +0630

    adding user signup

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
 db.connectToDatabase()
   .then(function () {
-    app.listen("3000");
+    app.listen(3000);
   })
   .catch(function (error) {
     console.log("Failed to connect to the database!");
-    console.log("error");
+    console.log(error);
   });
